01d3ebaa4740065cb097d3f6d83af94f2cfdf8e4
webstudying123 <752787611@qq.com>
2019-03-30 16:19:20 +0800

    '后台页面布局'

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,21 +1,55 @@
 <template>
-    <div>
-        <div class="index">
-            index
-        </div>
-    </div>
+  <el-container class="bigcontainer">
+    <!-- 头部 -->
+    <el-header class="my-header">
+      <el-row>
+        <el-col :span="4">
+          <img src="../assets/logo.png" alt>
+        </el-col>
+        <el-col :span="18">
+          <h2>后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <el-button type="success" round>退出</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container class="my-body">
+      <el-aside class="my-aside" width="200px">Aside</el-aside>
+      <el-main class="my-main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang='scss'>
- .index {
-     width: 100px;
-     height: 100px;
-     background-color: red;
- }
+.bigcontainer {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+    .my-header{
+        background-color: #b3c0d1;
+        height: 60px;
+        line-height: 60px;
+        h2 {
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+
+    }
+    .my-body{
+        flex: 1;
+        .my-aside{
+            background-color: white;
+        }
+        .my-main {
+            background-color: #e9eef3;
+        }
+    }
+}
 </style>
