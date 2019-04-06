047b3b208b577ca3a9aace3153bac864b8d1cd4f
webstudying123 <752787611@qq.com>
2019-04-06 20:36:00 +0800

    'category页面完成'

--- src/components/categories.vue
+++ src/components/categories.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="outbox">
     <!-- 导航 -->
-      <mybread twotitle='商品管理' threetitle='商品列表'></mybread>
+    <mybread twotitle="商品管理" threetitle="商品列表"></mybread>
     <!-- 输入框 -->
     <div class="inputbox">
       <el-col :span="6">
@@ -9,10 +9,24 @@
       </el-col>
     </div>
     <!-- 表格 -->
-    <el-table :data="catelist" style="width: 100%" border>
-      <el-table-column prop="username" label="分类名称" width="180"></el-table-column>
-      <el-table-column prop="email" label="级别" width="180"></el-table-column>
-      <el-table-column prop="mobile" label="是否有效"></el-table-column>
+    <el-table :data="catelist" style="width: 100%" border row-key='cat_id'>
+      <el-table-column prop="cat_name" label="分类名称" width="180"></el-table-column>
+      <el-table-column prop="cat_level" label="级别" width="180">
+        <template slot-scope="scope">
+         <span v-if='scope.row.cat_level==0'>一级</span>
+         <span v-else-if='scope.row.cat_level==1'>二级</span>
+          <span v-else>三级</span>
+        </template>
+      </el-table-column>
+
+      <!-- <el-table-column prop="cat_name" label="分类名称" width="180"></el-table-column> -->
+
+      <el-table-column prop="cat_deleted" label="是否有效">
+        <template slot-scope="scope">
+          <span v-if='scope.row.cat_deleted==false'>有效</span>
+          <span v-else>无效</span>
+        </template>
+      </el-table-column>
       <el-table-column prop="address" label="操作">
         <template slot-scope="scope">
           <el-button
@@ -44,12 +58,27 @@
 
 <script>
 export default {
-  name:'goods',
- data(){
-     return {
-         catelist:[{},{}]
-     }
- }
+  name: "categories",
+  data() {
+    return {
+      catelist: [{}, {}]
+     
+    };
+  },
+  methods: {
+    async getdata() {
+      let res = await this.$axios.get("categories", {
+        params: {
+          type: 3
+        }
+      });
+      console.log(res);
+      this.catelist = res.data.data;
+    }
+  },
+  created() {
+    this.getdata();
+  }
 };
 </script>
 
